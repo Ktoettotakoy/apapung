fd4b15cda4541dd8cdf3bc5d7fe4ac3d11b119c4
Ktoettotakoy <98839231+Ktoettotakoy@users.noreply.github.com>
2024-11-07 22:03:40 +0000

    initial setup

---
+++ backend/apapung/src/main/java/ie/tcd/scss/apapung/ApapungApplication.java
@@ -0,0 +1,13 @@
+package ie.tcd.scss.apapung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApapungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApapungApplication.class, args);
+	}
+
+}
