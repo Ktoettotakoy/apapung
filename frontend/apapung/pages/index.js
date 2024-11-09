1a87bfa2b760bdc402abbd199e9610c935a8c828
Yaqi66 <162325968+Yaqi66@users.noreply.github.com>
2024-11-09 05:46:38 +0000

    add gif to homePage

--- frontend/apapung/pages/index.js
+++ frontend/apapung/pages/index.js
@@ -1,12 +1,34 @@
+import Head from 'next/head';
 import styles from "../styles/Home.module.css";
 import TextInput from "../components/textInput.js";
 
 export default function Home() {
-  return (
-    <div>
-      Hello World
-      <TextInput></TextInput>
-      <TextInput></TextInput>
-    </div>
-  );
+    return (
+        <>
+            {/* Meta and Page Head */}
+            <Head>
+                <title>Apapung</title>
+                <link rel="icon" type="image/jpg" href="/favicon.ico" />
+            </Head>
+
+            {/* Main Content */}
+            <main className={styles.main}>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', paddingTop: '10vh' }}>
+                    <img src="/pokemonTitle.gif" alt="Pokemon Title" width="400" />
+                </div>
+
+                <div style={{ textAlign: 'center', marginTop: '20px' }}>
+                    <img src="/Picacku.gif" alt="Pikachu" width="500" />
+                </div>
+
+                <h1 style={{ textAlign: 'center', marginTop: '20px' }}>
+                    Welcome to Apapung
+                </h1>
+
+                <TextInput />
+            </main>
+        </>
+    );
 }
+
+
