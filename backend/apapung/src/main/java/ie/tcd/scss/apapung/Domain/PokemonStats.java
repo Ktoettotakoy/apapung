bd2dce77606496180659a4c09a4b42a712e96697
mooneybb <mooneybb@tcd.ie>
2024-11-09 23:42:10 +0000

    Added variables to class

--- backend/apapung/src/main/java/ie/tcd/scss/apapung/Domain/PokemonStats.java
+++ backend/apapung/src/main/java/ie/tcd/scss/apapung/Domain/PokemonStats.java
@@ -3,13 +3,40 @@ package ie.tcd.scss.apapung.Domain;
 import java.util.List;
 
 public class PokemonStats {
-    List<Stat> stats;
+    private String name;
+    private List<String> types;
+    private int dexNumber;
+    private List<Stat> stats;
+    private String spriteURL;
 
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<String> getTypes() {
+        return types;
+    }
+    public void setTypes(List<String> types) {
+        this.types = types;
+    }
+    public int getDexNumber() {
+        return dexNumber;
+    }
+    public void setDexNumber(int dexNumber) {
+        this.dexNumber = dexNumber;
+    }
     public List<Stat> getStats() {
         return stats;
     }
-
     public void setStats(List<Stat> stats) {
         this.stats = stats;
+    }
+    public String getSpriteURL() {
+        return spriteURL;
+    }
+    public void setSpriteURL(String spriteURL) {
+        this.spriteURL = spriteURL;
     }
 }
