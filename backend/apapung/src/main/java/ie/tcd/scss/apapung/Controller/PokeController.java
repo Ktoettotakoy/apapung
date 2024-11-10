158f218efbeb138384fd0d2fe630b7a80bb88da5
Ktoettotakoy <98839231+Ktoettotakoy@users.noreply.github.com>
2024-11-10 12:35:10 +0000

    updated the poke controller so that types are list of strings now

--- backend/apapung/src/main/java/ie/tcd/scss/apapung/Controller/PokeController.java
+++ backend/apapung/src/main/java/ie/tcd/scss/apapung/Controller/PokeController.java
@@ -3,6 +3,7 @@ package ie.tcd.scss.apapung.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/pokemon")
 public class PokeController {
 
@@ -30,7 +32,6 @@ public class PokeController {
         if (!pokeService.isValidName(pokemon)) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("Error", "Pokemon name invalid"));
         }
-
         // return result
         return ResponseEntity.ok(this.pokeService.getPokeApiStats(pokemon));
 
